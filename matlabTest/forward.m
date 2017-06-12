c3c94041cadd43c9bef423811f989b73bc3ee502
lwj456456 <zerg1355@gmail.com>
2017-06-12 13:03:50 +0900

    delete all

--- matlabTest/forward.m
+++
@@ -1,8 +0,0 @@
-function [b_l_d,b_h_d,grp] = forward(lpt,hpt,b_l,f_delay,grp)
-
-b_l_t=  conv(lpt,b_l);
-b_l_d= b_l_t(1:2:end);
-b_h_t = conv(hpt,b_l);
-b_h_d= b_h_t(1:2:end);
-grp = (grp+f_delay)/2;
-end
--- matlabTest/quant.m
+++
@@ -1,7 +0,0 @@
-function [bq, eq] = quant(signal,n)
-
-tb = max(signal)-min(signal);
-tt = round(signal*2^n/tb);
-bq = tt/2^n*tb;
-eq = sum(sqrt((signal-bq).^2));
-end
